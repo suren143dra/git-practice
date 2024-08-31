b0936e0c2b443f6e8cbbfcd0020a307f63f9dd58
Surendra Reddy <surendrareddyrajula1@gmail.com>
2024-09-01 00:43:29 +0530

    I am commiting Discussion file

---
+++ 02-discussion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Author  Surendra Reddy
+echo "Surendra:: Hi Anu, How are doing"
+echo "Anu:: I am goood Surendra, Thanks. how about you"
+echo  "Surendra:: I am fine, what's going on"
+echo "Anu:: I am learing AI Tools"
